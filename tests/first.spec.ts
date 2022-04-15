c7a64be28c1ee5ec06c56449752b323a1b51004e
Alex Prokopovich <avs@live.ru>
2022-04-15 12:29:52 +0500

    fixes

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test'
+import { allure } from "allure-playwright";
 
 test.describe('two tests one', () => {
   test('1 > 1 @smoke', async({ page }) => {
+    allure.issue({
+      url: "https://github.com/allure-framework/allure-js/issues/352",
+      name: "Target issue",
+    });
     await page.goto('https://playwright.dev/')
     await expect(page).toHaveTitle(/Playwright/)
     // Compare screenshot with a stored reference.
